7a6605a96fd682aa46dbe08552d1088dbe5f0fcc
Samarth Shete <46786711+samarthshete@users.noreply.github.com>
2025-10-09 16:43:32 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/samarthshete/', 
+  base: '/portfolio/', 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
